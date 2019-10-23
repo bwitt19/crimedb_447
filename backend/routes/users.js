29a3971957a37d940c1e8e86a1c25d2d0bf454bc
Aaron Grodzinsky <aarongrodzinsky@gmail.com>
2019-10-23 15:30:56 -0400

    Added .get and .post endpoints to check if an account exists and to create an account

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,7 +5,7 @@ let User = require('../models/user.model');
 router.route('/').get((req, res) => {
     
     // Check to see if we can find an account with a matching username and password
-    User.find({
+    User.findOne({
         'user': { req.body.user },
         'pass': { req.body.pass }
     }) 
@@ -13,6 +13,25 @@ router.route('/').get((req, res) => {
     // If the user exists, return their filters that they have saved
     .then(users => res.json(users.filters))
     .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// .post endpoint to add a new user to the database
+router.route('/').post((req, res) => {
+    
+    // Create the new user Schema
+    var user = new User({
+        user: req.body.user,
+        pass: req.body.pass,
+        filters: []
+    });
+        
+    // Save it to the database
+    user.save(function (err, retUser) {
+        if (err) return console.error(err);
+        
+        console.log(retUser._id + "Saved to account collection");
+    }    
+    
 });
 
 module.exports = router;
