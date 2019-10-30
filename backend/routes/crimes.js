31dec8a38d99495581cebb688d41a6c2b7683ab9
jfield1 <jfield1@umbc.edu>
2019-10-30 13:20:34 -0400

    add foundation for 'filter?' git route using req.query

--- backend/routes/crimes.js
+++ backend/routes/crimes.js
@@ -1,20 +1,54 @@
 const router = require('express').Router();
 let Crime = require('../models/crime.model');
 
-// crimes route endpoints 
+// --Crimes route endpoints 
 
-// .get endpoint will get first 5, for testing
-router.route('/').get((req, res) => {
-    console.log('/');
-    Crime.find().limit(5)
-    .then(crimes => res.json(crimes))
-    .catch(err => res.status(400).json('Error: ' + err));
+// --get query endpoint
+router.route('/api/filter').get((req, res) => {
+    console.log(`get: ${JSON.stringify(req.query)}`);
+
+    // --Range of data filters
+    if ( typeof req.query.date != 'undefined' || 
+        typeof req.query.time != 'undefined' ||
+        typeof req.query.long != 'undefined' ||
+        typeof req.query.lat != 'undefined'
+    ) {
+        
+        // parse into a passed range to mongo find
+        Crime.find(
+            req.query
+            ).limit(100)
+        .then(crimes => res.json(crime))
+        .catch(err => res.status(400).json('Error: ' + err));
+    } 
+    // --Simple filters
+    else if ( typeof req.query.neighborhood != 'undefined' || 
+        typeof req.query.crimecode != 'undefined' ||
+        typeof req.query.premise != 'undefined' ||
+        typeof req.query.premise != 'undefined' ||
+        typeof req.query.district != 'undefined' ||
+        typeof req.query.weapon != 'undefined' ||
+        typeof req.query.type != 'undefined'
+    ) {
+        Crime.find( 
+            
+            req.query
+            // TODO: add multiple values for one field (knife or firearm, ie checklist)
+
+        ).limit(100)
+        .then(crimes => res.json(crimes))
+        .catch(err => res.status(400).json('Error: ' + err));
+    } else {
+        // TODO: Return query error
+    }
 });
 
 
-// .post filtering endpoint
-router.route('/').post((req, res) => {
+// .post filtering endpoint (TODO: remove)
+router.route('/field/:field/value/:value').post((req, res) => {
 
+    const field = req.params.field;
+    console.log(`test ${field}`);
     const date = new Date(req.body.date);
     
     var year=date.getYear();
