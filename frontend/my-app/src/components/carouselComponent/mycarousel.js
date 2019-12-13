9c5951ffdbf437be7eb17cac9b5a37f99e2141f1
Aaron Grodzinsky <aarongrodzinsky@gmail.com>
2019-12-13 15:40:59 -0500

    Added basic heatmap functionality

--- frontend/my-app/src/components/carouselComponent/mycarousel.js
+++ frontend/my-app/src/components/carouselComponent/mycarousel.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import { Map, TileLayer } from 'react-leaflet';
+import Leaflet from 'leaflet'
+import HeatmapLayer from 'react-leaflet-heatmap-layer';
+import 'leaflet/dist/leaflet.css';
 
 class myCarousel extends Component {
 
@@ -8,21 +12,33 @@ class myCarousel extends Component {
   }
 
     render() {
-        return (
+
+      const topCorner = Leaflet.latLng(39.404897, -76.838722)
+      const bottomCorner = Leaflet.latLng(39.179312, -76.389313)   
+      const bounds = Leaflet.latLngBounds(topCorner, bottomCorner)
+
+      return (
           <div class="my-carousel">
-            <Carousel>
+            <Carousel interval={false}>
               <Carousel.Item>
-                <div class="carousel-slide"></div>                 
+                <div class="carousel-slide">
+
+                  <Map id="heatmap" center={[39.2904, -76.6122]} zoom={12} minZoom={12}  maxBoundsViscosity={0.5} maxBounds={bounds}>         
+                    <TileLayer attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />        
+                  </Map> 
+
+                </div>                 
               </Carousel.Item>
-              <Carousel.Item>
+              
+               <Carousel.Item>
                 <div class="carousel-slide" style={{background:'green'}}></div>                 
               </Carousel.Item>
               <Carousel.Item>
                 <div class="carousel-slide" style={{background:'red'}}></div>                 
-              </Carousel.Item>
+              </Carousel.Item> 
             </Carousel>
 
-	    {JSON.stringify(this.props.data, null, 2) } 
+	    {/* {JSON.stringify(this.props.data, null, 2) }   */}
           </div>
         );
 
