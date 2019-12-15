937ad2e910baa0af0fb7e297f6c7446c6e79276a
Ubuntu <ubuntu@ip-172-31-37-126.ec2.internal>
2019-12-15 15:43:37 +0000

    Updated heatmap

--- frontend/my-app/src/components/carouselComponent/mycarousel.js
+++ frontend/my-app/src/components/carouselComponent/mycarousel.js
@@ -8,8 +8,13 @@ import 'leaflet/dist/leaflet.css';
 class myCarousel extends Component {
 
   constructor(props){
-    super(props);
+      super(props);
+      this.state = {currSlide: 0, sliding: false};
   }
+
+    changeSlide(newSlide) {
+	this.setState({currSlide: newSlide, sliding: true});
+    }
 
     render() {
 
@@ -17,29 +22,38 @@ class myCarousel extends Component {
       const bottomCorner = Leaflet.latLng(39.179312, -76.389313)   
       const bounds = Leaflet.latLngBounds(topCorner, bottomCorner)
       const addressPoints = [];
-
+      const gradient = {0.2: 'blue', 0.3: 'cyan', 0.5:'#aeff00',0.6:'#ffbf00',0.8:'#ff8000', 0.9: '#ff4d00', 1.0: '#ff0000'};
+	
+      // Push the datapoints into the heatmap layer
       for (var x= 0; x < this.props.data.length; x++) {
-        addressPoints.push([this.props.data[x].latitude, this.props.data[x].longitude, "as"])
-        //alert(addressPoints[x])
+        addressPoints.push([this.props.data[x].latitude, this.props.data[x].longitude, 0.1])
       }
       
       return (
           <div class="my-carousel">
-              <Carousel interval={false} touch={false}>
+              <Carousel interval={false} slide={false} touch={false} onSelect={this.changeSlide.bind(this)} >
               <Carousel.Item>
                 <div class="carousel-slide">
 
+	  {this.state.currSlide != 0 ?
+	   (<div></div>) : (
+	  
                   <Map id="heatmap" center={[39.2904, -76.6122]} zoom={12} minZoom={12}  maxBoundsViscosity={0.5} maxBounds={bounds}>         
-                  <HeatmapLayer
-            fitBoundsOnLoad
-            fitBoundsOnUpdate
-            points={addressPoints}
-            longitudeExtractor={m => m[1]}
-            latitudeExtractor={m => m[0]}
-            intensityExtractor={m => parseFloat(m[2])} />
-                    <TileLayer attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />        
+              <HeatmapLayer
+          fitBoundsOnLoad
+          fitBoundsOnUpdate
+          points={addressPoints}
+          longitudeExtractor={m => m[1]}
+          latitudeExtractor={m => m[0]}
+          intensityExtractor={m => parseFloat(m[2])}
+	  max={0.5}
+	  blur={30}
+	  gradient={gradient}
+	  minOpacity={0.75}
+	      />
+              <TileLayer attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />        
                   </Map> 
-
+	   )}
                 </div>                 
               </Carousel.Item>
               
